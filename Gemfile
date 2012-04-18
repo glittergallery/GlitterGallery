d33ea4d5cfd83c8af1f2ba1731bf2549578755cb
Emily Dirsh <edirsh@redhat.com>
2012-04-17 21:37:15 -0400

    added haml and devise for user management

--- Gemfile
+++ Gemfile
@@ -8,6 +8,8 @@ gem 'rails', '3.2.3'
 gem 'mysql2'
 gem 'sqlite3'
 gem 'bigdecimal'
+gem 'devise'
+gem 'haml'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Glitter::Application.routes.draw do
+  devise_for :users
+
   get "home/index"
 
   # The priority is based upon order of creation:
@@ -50,7 +52,7 @@ Glitter::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
