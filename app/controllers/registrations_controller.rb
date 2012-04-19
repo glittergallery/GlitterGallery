cee5e6a98b67a22d640451fbd2613e0a95f4c8ce
Emily Dirsh <edirsh@redhat.com>
2012-04-19 19:19:35 -0400

    more error detecting/handling around repo initialization

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,7 +3,11 @@ class RegistrationsController < Devise::RegistrationsController
   def repo_init 
     if user_signed_in?
       r = Repo.new current_user.repo 
-      r.init
+      begin
+        r.init
+      rescue
+        flash[:error] = "Unable to init your repo"
+      end
       flash[:notice] = "Your repo has been inited!"
       redirect_to after_sign_in_path_for(current_user)
     end
