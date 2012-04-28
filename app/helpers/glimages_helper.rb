8b820ff2f8efe562978e279dc73a735c2467636c
Emily Dirsh <edirsh@redhat.com>
2012-04-27 21:57:35 -0400

    change public repo symlink to be a bit more convenient for linking

--- app/helpers/glimages_helper.rb
+++ app/helpers/glimages_helper.rb
@@ -3,9 +3,9 @@ module GlimagesHelper
   # pass in Glimage object
   def show_image(image)
     if image.is_svg?
-      content_tag(:object, :data => "/static/#{image.imagepath}") {}
+      content_tag(:object, :data => "#{image.imagepath}") {}
     else
-      tag :image, :src => "/static/#{image.imagepath}"
+      tag :image, :src => "#{image.imagepath}"
     end
   end
 end
