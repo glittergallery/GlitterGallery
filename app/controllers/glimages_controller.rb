2f9d7805e38c828df3c67f3ac03bf1e49a6c3400
Emily Dirsh <edirsh@redhat.com>
2012-04-30 21:58:48 -0400

    Added new image option on project page

--- app/controllers/glimages_controller.rb
+++ app/controllers/glimages_controller.rb
@@ -1,5 +1,19 @@
 class GlimagesController < ApplicationController
   
+  def create
+    img = Glimage.new :file => params[:glimage][:file].original_filename
+    project = Project.find(params[:glimage][:project_id])
+    img.project_id = project.id
+    if img.save
+      image_commit project, params[:glimage][:file]
+      create_thumbnail img
+      flash[:notice] = 'Your image was saved! How sparkly!'
+    else
+      flash[:error] = "Something went wrong, your image didn't get saved - how sad."
+    end
+    redirect_to url_for(project)
+  end
+
   def show
     @glimage = Glimage.find params[:id]
     logger.debug "Glimage: #{@glimage.inspect}"
