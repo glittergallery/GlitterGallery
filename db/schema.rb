6ac5f64319841bedce79068cad53e43dd38dd7fd
Máirín Duffy <duffy@redhat.com>
2012-05-10 23:31:44 -0400

    some style tweaks to the new private? checkbox

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120503173742) do
+ActiveRecord::Schema.define(:version => 20120506194333) do
 
   create_table "comments", :force => true do |t|
     t.string   "author"
@@ -29,6 +28,7 @@ ActiveRecord::Schema.define(:version => 20120503173742) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.string   "filetype"
+    t.boolean  "private"
   end
 
   create_table "projects", :force => true do |t|
