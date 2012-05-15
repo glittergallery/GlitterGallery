f09ed3bacce9d2b3a7ad3764f69b5084dc8f7b77
Emily Dirsh <emily@redhat.com>
2012-05-14 22:25:55 -0400

    WIP: Styled and linkified gallery

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     # Find all public images
-    @glimages = Glimage.where("private is not ?", true).order('updated_at DESC').page(params[:page]).per_page(4)
+    @glimages = Glimage.where("private is not ?", true).order('updated_at DESC').page(params[:page]).per_page(6)
 
     # if user_signed_in?
     #   redirect_to dashboard_path
