6f029765a9d45e2314ac2168108a777d85cef739
Emily Dirsh <emily@redhat.com>
2012-06-04 01:19:42 -0400

    Remove unnecessary function in Glimages

--- app/models/glimage.rb
+++ app/models/glimage.rb
@@ -10,23 +10,12 @@ class Glimage < ActiveRecord::Base
     filetype == 'image/svg+xml' || file.split('.').pop == 'svg'
   end
 
-  def build_parents
-    if @project.nil?
-      @project = Project.find(project_id)
-    end
-    if @user.nil?
-      @user= User.find(@project.user_id)
-    end
-  end
-
   def filepath
-    build_parents
-    File.join(@project.path, file)
+    File.join(project.path, file)
   end
 
   def imagepath
-    build_parents
-    File::SEPARATOR + File.join('repos', @user.email, @project.name, file)
+    File::SEPARATOR + File.join('repos', project.user.email, project.name, file)
   end
 
   # Returns thumbnail path
@@ -38,7 +27,7 @@ class Glimage < ActiveRecord::Base
   end
 
   def belongs_to?(user)
-    user.projects.include?(Project.find(project_id))
+    user.projects.include?(project)
   end
   
 end
