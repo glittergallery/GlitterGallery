3b2300ce6f89806951fe98f0af7fb18cb4b4630e
Sarup Banskota <sbanskota08@gmail.com>
2013-07-13 04:53:25 +0530

    Add sessions controller to handle OpenID login and update Gemfile

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,10 +3,12 @@ class HomeController < ApplicationController
     # Find all public images
     @glimages = Glimage.where("private is not ?", true).order('updated_at DESC').page(params[:page]).per_page(6)
 
-    # if user_signed_in?
-    #   redirect_to dashboard_path
-    # else
-    #   redirect_to new_user_session_path
-    # end
+    if user_signed_in?
+      redirect_to dashboard_path
+    else
+      redirect_to new_user_session_path
+    end
   end
 end
+
+
--- app/controllers/repo_controller.rb
+++ app/controllers/repo_controller.rb
@@ -1,5 +1,5 @@
 class RepoController < ApplicationController
-  before_filter :authenticate_user!, :except => :show
+#  before_filter :authenticate_user!, :except => :show
 
   def index
     @user = current_user
