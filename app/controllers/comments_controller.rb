b025ba652d0961cb4e9368ab597daf282607c02a
Sarup Banskota <sbanskota08@gmail.com>
2013-07-14 00:18:39 +0530

    Update comments comments controller to work for polycomments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,31 @@
 class CommentsController < ApplicationController
+  before_filter :load_polycomment
+
+  def index
+    @comments = @polycomment.comments
+  end
+
+  def new
+    @comment = @polycomment.comments.new
+  end
+
   def create
-    comment = Comment.new :author => params[:comment][:author], 
-                          :email => params[:comment][:email].strip,
-                          :body => params[:comment][:body]
-    comment.glimage_id = params[:comment][:glimage_id]
-    if comment.save
+    @comment = @polycomment.comments.new(body: params[:comment][:body], 
+                                         issue: false)
+    @comment.user_id = current_user.id
+    if @comment.save
       flash[:notice] = 'Your comment was posted!'
+      redirect_to @polycomment
     else
       flash[:alert] = 'Something went wrong, try reposting your comment.'
     end
-    img = Glimage.find(comment.glimage_id)
-    redirect_to url_for(img)
   end
+
+
+  private
+
+    def load_polycomment
+      resource, id = request.path.split('/')[1, 2]
+      @polycomment = resource.singularize.classify.constantize.find(id)
+    end
 end
