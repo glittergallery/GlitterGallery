4ef63ddc593f0500c378877e21fde437698b207e
Sarup Banskota <sbanskota08@gmail.com>
2013-07-18 19:15:20 +0530

    Add glitterpost specs and factories, remove tests folder

---
+++ spec/controllers/glitterposts_controller_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe GlitterpostsController do
+
+  describe "GET 'new'" do
+    it "returns http success" do
+      get 'new'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'create'" do
+    it "returns http success" do
+      get 'create'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'update'" do
+    it "returns http success" do
+      get 'update'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'destroy'" do
+    it "returns http success" do
+      get 'destroy'
+      response.should be_success
+    end
+  end
+
+end
