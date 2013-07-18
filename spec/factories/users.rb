e6e746f6b02ddf4be2862bd7687403ed631025bd
Sarup Banskota <sbanskota08@gmail.com>
2013-07-18 19:18:35 +0530

    Update user spec and factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,9 @@
-require 'faker'
+# Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :user do |f|
-    f.email { Faker::Internet.email }
-    f.password 'secret'
-    f.password_confirmation 'secret'
+  factory :user do
+    email "sbanskota08@gmail.com"
+    identity_url "sarupbanskota.id.fedoraproject.org"
+    username "sarupbanskota"
   end
 end
