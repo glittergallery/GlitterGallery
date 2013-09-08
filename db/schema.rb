18cb9655ee8b5014cf8fde4f659e751a5d1a92d8
Sarup Banskota <sbanskota08@gmail.com>
2013-09-08 17:09:33 +0530

    Add comments back in. We're indexing on polycomment_id(string) this time

--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130902134457) do
+ActiveRecord::Schema.define(:version => 20130908113252) do
 
   create_table "comments", :force => true do |t|
     t.text     "body"
-    t.integer  "responding_to"
-    t.datetime "created_at",       :null => false
-    t.datetime "updated_at",       :null => false
-    t.integer  "polycomment_id"
+    t.string   "polycomment_id"
     t.string   "polycomment_type"
     t.integer  "user_id"
     t.boolean  "issue"
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
   end
+
+  add_index "comments", ["polycomment_id"], :name => "index_comments_on_polycomment_id"
 
   create_table "glitterposts", :force => true do |t|
     t.string   "title"
