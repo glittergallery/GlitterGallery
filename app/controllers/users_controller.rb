d7bd65ceb8100c63cc0bdb7af0745a27ad4b3431
Sarup Banskota <sbanskota08@gmail.com>
2013-10-02 11:37:42 +0530

    Add user controller, add back hidden fork action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,25 @@
-def show
-  @user = User.find(params[:id])
-end
+class UsersController < ApplicationController
 
-def new
-  @user = User.new
-end
+	def show
+	  @user = User.find(params[:id])
+	  @email = @user.email 
+	  @username = @user.username
+	  # TODO - Similarly extract other user details,
+	  #        and show them up in the view.
+	end
 
-def create
-  @user = User.new(params[:user])
-  if @user.save
-  	login @user
-  	redirect_to dashboard_url
-  else
-  	render 'new'
-  end
+	def new
+	  @user = User.new
+	end
+
+	def create
+	  @user = User.new(params[:user])
+	  if @user.save
+	  	login @user
+	  	redirect_to dashboard_url
+	  else
+	  	render 'new'
+	  end
+	end
+
 end
