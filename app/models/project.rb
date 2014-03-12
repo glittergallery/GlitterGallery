3aa3e8be6e7fb19ef6ec2b060e406ff6910f2818
Charles Xue <charlesx2013@gmail.com>
2014-03-12 15:17:15 -0700

    Fixed unqiueness issue

--- app/models/project.rb
+++ app/models/project.rb
@@ -7,7 +7,7 @@ class Project < ActiveRecord::Base
 
   attr_accessible :name, :path, :parent
 
-  validates :name, :presence => true
+  validates :name, :presence => true, uniqueness: true
 
   # get the last update time
   # of the images in the project
