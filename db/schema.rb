a5cde2b91266b911b65656e409ffa90db6b1c6e9
Sarup Banskota <sbanskota08@gmail.com>
2014-03-13 21:17:08 +0530

    couple of things deprecated in rails 4

--- db/schema.rb
+++ db/schema.rb
@@ -9,73 +9,73 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131104181756) do
+ActiveRecord::Schema.define(version: 20131104181756) do
 
-  create_table "comments", :force => true do |t|
+  create_table "comments", force: true do |t|
     t.text     "body"
     t.string   "polycomment_id"
     t.string   "polycomment_type"
     t.integer  "user_id"
     t.boolean  "issue"
-    t.datetime "created_at",       :null => false
-    t.datetime "updated_at",       :null => false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
   end
 
-  add_index "comments", ["polycomment_type", "polycomment_id"], :name => "index_comments_on_polycomment_type_and_polycomment_id"
+  add_index "comments", ["polycomment_type", "polycomment_id"], name: "index_comments_on_polycomment_type_and_polycomment_id"
 
-  create_table "glitterposts", :force => true do |t|
+  create_table "glitterposts", force: true do |t|
     t.string   "title"
     t.text     "content"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer  "user_id"
   end
 
-  create_table "projects", :force => true do |t|
+  create_table "projects", force: true do |t|
     t.string   "name"
     t.integer  "repo_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer  "user_id"
     t.string   "path"
     t.integer  "parent"
   end
 
-  add_index "projects", ["name", "user_id"], :name => "index_projects_on_name_and_user_id", :unique => true
+  add_index "projects", ["name", "user_id"], name: "index_projects_on_name_and_user_id", unique: true
 
-  create_table "pull_requests", :force => true do |t|
+  create_table "pull_requests", force: true do |t|
     t.string   "desc"
     t.string   "lastcommit"
     t.string   "status"
     t.integer  "parent"
     t.integer  "fork"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "pulltable", :force => true do |t|
+  create_table "pulltable", force: true do |t|
     t.string   "desc"
     t.string   "status"
     t.string   "lastcommit"
     t.integer  "fork"
     t.integer  "parent"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "users", :force => true do |t|
+  create_table "users", force: true do |t|
     t.string   "email"
-    t.datetime "created_at",     :null => false
-    t.datetime "updated_at",     :null => false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
     t.string   "identity_url"
     t.string   "username"
     t.string   "remember_token"
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["identity_url"], :name => "index_users_on_identity_url", :unique => true
-  add_index "users", ["remember_token"], :name => "index_users_on_remember_token", :unique => true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["identity_url"], name: "index_users_on_identity_url", unique: true
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token", unique: true
 
 end
