faa238b801a069f691249635d28c04f0f29a60c2
Rohit Paul Kuruvilla <rohitpaul@iitrpr.ac.in>
2014-03-17 14:41:41 +0530

    CommentsControllerSpec

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -2,7 +2,7 @@ require 'faker'
 
 FactoryGirl.define do
   factory :project do |f|
-    f.name { Faker::Lorem.words(2).join("") }
+    f.name { Faker::Lorem.words(2).join(" ") }
     f.association :user
   end
 end
