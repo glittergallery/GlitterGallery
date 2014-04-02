44db133d92d94e5d00ae32b0c6e5d1754b246f59
Sarup Banskota <sbanskota08@gmail.com>
2014-04-02 16:39:05 +0200

    Remove public/assets, it interferes and loads the js twice. When we need it, we checkout to this point again

--- public/assets/extensions/ext-helloworld.js
+++
@@ -1,9 +0,0 @@
-/*
- * ext-helloworld.js
- *
- * Licensed under the MIT License
- *
- * Copyright(c) 2010 Alexis Deveria
- *
- */
-svgEditor.addExtension("Hello World",function(){return{name:"Hello World",svgicons:"/assets/extensions/helloworld-icon.xml",buttons:[{id:"hello_world",type:"mode",title:"Say 'Hello World'",events:{click:function(){svgCanvas.setMode("hello_world")}}}],mouseDown:function(){return"hello_world"==svgCanvas.getMode()?{started:!0}:void 0},mouseUp:function(t){if("hello_world"==svgCanvas.getMode()){var e=svgCanvas.getZoom(),i=t.mouse_x/e,n=t.mouse_y/e,a="Hello World!\n\nYou clicked here: "+i+", "+n;$.alert(a)}}}});
