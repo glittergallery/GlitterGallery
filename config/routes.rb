aec96c4ebc711a382b0654e18985329a8f5750b9
Sarup Banskota <sbanskota08@gmail.com>
2014-04-11 22:49:35 +0530

    Bring the private stuff to more pages, create routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,23 @@ Glitter::Application.routes.draw do
 
   post 'glitterposts/:id/edit' => 'glitterposts#update'
 
+  get 'projects/:id/:xid' => 'projects#show'
+  get 'projects/:id/:xid/commits' => 'projects#commits'
+  get 'projects/:id/:xid/commit/:tree_id' => 'projects#projectcommit'
+  get 'projects/:id/:xid/masterbranch/:image_name' => 'projects#masterbranch', :image_name => /[^\/]*/
+  get 'projects/:id/:xid/masterbranch/:image_name/history' => 'projects#file_history', :image_name => /[^\/]*/
+  get 'projects/:id/:xid/createsvg' => 'projects#new_svg'
+  get 'projects/:id/:xid/newfile' => 'projects#newfile'
+  get 'projects/:id/:xid/masterbranch/:image_name/edit' => 'projects#edit_svg', :image_name => /[^\/]*/
+  get 'projects/:id/:xid/masterbranch/:image_name/update' => 'projects#update', :image_name => /[^\/]*/
+  get 'projects/:id/:xid/fork' => 'projects#fork'
+  get 'projects/:id/:xid/forkyou' => 'projects#forkyou'
+  get 'projects/:id/:xid/pull' => 'projects#pull_request'
+  get 'projects/:id/:xid/pull/:pull_id' => 'projects#pull'
+  get 'projects/:id/:xid/pull/:pull_id/merge' => 'projects#merge'
+  get 'projects/:id/:xid/pull/:pull_id/close' => 'projects#close'
+  get 'projects/:id/:xid/pull/:pull_id/open' => 'projects#open'
+  get 'projects/:id/:xid/pulls' => 'projects#pulls'
 
   get 'projects/:id/commits' => 'projects#commits'
   get 'projects/:id/commit/:tree_id' => 'projects#projectcommit'
@@ -24,7 +41,6 @@ Glitter::Application.routes.draw do
   get 'projects/:id/pull/:pull_id/merge' => 'projects#merge'
   get 'projects/:id/pull/:pull_id/close' => 'projects#close'
   get 'projects/:id/pull/:pull_id/open' => 'projects#open'
-
   get 'projects/:id/pulls' => 'projects#pulls'
   
   resources :projects do
