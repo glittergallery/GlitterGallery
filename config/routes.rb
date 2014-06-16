bbaaca1fd613d5cbbe1117cc57905bfef4a25f33
Sarup Banskota <sbanskota08@gmail.com>
2014-06-17 03:37:12 +0530

    Update some user related pages

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Glitter::Application.routes.draw do
   get '/dashboard' => 'dashboard#index', :as => :dashboard
   get 'settings' => 'users#settings', :as => :user_settings
   get '/:username' => 'users#show'
+  get '/:username/projects' => 'users#list_projects'
   get '/:username/:project' => 'projects#show'
   get '/:username/:project/commits' => 'projects#commits'
   get '/:username/:project/commit/:tree_id' => 'projects#projectcommit'
