7f08ee2ea468df80116442b62116123f742d3884
Rohit Paul Kuruvilla <rohitpaul@iitrpr.ac.in>
2014-06-22 16:58:25 +0530

    Refactored tests around new login design

--- spec/features/auth_spec.rb
+++ spec/features/auth_spec.rb
@@ -3,32 +3,32 @@ require 'spec_helper'
 feature "Authentication" do
   scenario "New user signs up via email" do
       visit "/"
-      click_link "Sign up"
+      click_link "Sign Up?"
       fill_in "user_email", :with => "sbanskota08@gmail.com"
       fill_in "user_password", :with => "secret12345"
       fill_in "user_password_confirmation", :with => "secret12345"
       fill_in "user_username", :with => "sarupbanskota"
-      click_button "Sign up"
-      expect(page.current_path).to eq("/projects/new")    
+      click_button "Sign up!"
+      expect(page.current_path).to eq("/dashboard")    
   end
   scenario "New user signs up via omniauth" do
       visit "/"
-      click_link "Facebook"
-      expect(page.current_path).to eq("/projects/new")  
+      click_button "Facebook"
+      expect(page.current_path).to eq("/dashboard")  
   end
   scenario "Existing user signs in via email" do
     @user = FactoryGirl.create(:user)
     visit "/"
-    click_link "Sign in"
+    #click_link "Sign in"
     fill_in "user_email", :with => @user.email
     fill_in "user_password", :with => "secret12345"   
-    click_button "Sign in"
-    expect(page.current_path).to eq("/projects/new")
+    click_button "Login"
+    expect(page.current_path).to eq("/dashboard")
   end
   scenario "Existing user signs up via omniauth" do
     @user = FactoryGirl.create(:user)
     visit "/"
-    click_link "Facebook"
-    expect(page.current_path).to eq("/projects/new")  
+    click_button "Facebook"
+    expect(page.current_path).to eq("/dashboard")  
   end
 end
