3edbb4a725cdeee1dd5910a779f2804cff59c08c
Sarup Banskota <sbanskota08@gmail.com>
2014-07-14 02:37:10 +0530

    Remove navbar from login page

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,9 +1,14 @@
 NProgress.configure({ showSpinner: false, speed: 700});
 
 // Based on which page you're on,
-// make relevant toolbar element active
+// make relevant toolbar element active,
+// remove navbar from login page
+
 $("document").ready( function(){
-	var articleContent = $("article").attr("data");
+	var articleContent = $(".wrapper article").attr("data");
+	if(articleContent === "login"){
+		$(".wrapper nav").css("display", "none");
+	}
 	var toolbarDivs = $("section.toolbar div, section.action div");
 	toolbarDivs.each(function(){
 			if($(this).attr("data") === articleContent){
