993ac4b90ad08822fc5a45c5ffb38c90153ad6d0
Sarup Banskota <sbanskota08@gmail.com>
2015-01-25 12:33:18 +0530

    Disable social login when env var is nil

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,28 +1,20 @@
 module SessionsHelper
-  # def login(user)
-  #   session[:current_user_id]=user.id
-  #   redirect_back()
-  # end
-  
-  # def current_user
-  #   @current_user ||= User.find(session[:current_user_id]) if session[:current_user_id]
-  #   rescue ActiveRecord::RecordNotFound
-  # end
-   
-  # def logged_in?
-  #   !current_user.nil?
-  # end
-   
-  # def logout
-  #   session[:current_user_id]=nil      
-  # end
 
-  # def authenticate_user!
-  #   if !current_user
-  #     flash[:alert] = 'You need to login before accessing this page!'
-  #     redirect_to login_path
-  #   end
-  # end 
+  def facebook?
+    true if ENV["FACEBOOK_KEY"] and ENV["FACEBOOK_SECRET"]
+  end
+
+  def github?
+    true if ENV["GITHUB_KEY"] and ENV["GITHUB_SECRET"]
+  end
+
+  def linkedin?
+    true if ENV["LINKEDIN_KEY"] and ENV["LINKEDIN_SECRET"]
+  end
+
+  def twitter?
+    true if ENV["TWITTER_KEY"] and ENV["TWITTER_SECRET"]
+  end
 
   def store_return_to
     session[:return_to] = request.url
