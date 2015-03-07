a99d9953dae20e5364a7211a944b1358bcfba1d4
Shrey Bagroy <shrey14099@iiitd.ac.in>
2015-03-07 14:40:00 +0530

    Minor changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -21,6 +21,7 @@ Glitter::Application.configure do
 
   # Raise an error on page load if there are pending migrations
   config.active_record.migration_error = :page_load
+  
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
