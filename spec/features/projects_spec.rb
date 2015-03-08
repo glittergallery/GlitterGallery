102ce75dac70998f4cc9421686f7bfa1c48e5c5b
Ahmed AbouElhamayed <alwahsh.ahmed@gmail.com>
2015-03-08 06:38:17 +0200

    fix comments on logs temporarily

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -33,22 +33,7 @@ feature "Projects" do
     expect(page).to have_content('testproject1')
   end
 
-  scenario "User uploads file and thumbnail is created" do
-    sign_up_with("t@test.com","test1","secret12345")
-    click_button "Create first project!"
-    fill_in "project_name", :with => "testproject1"
-    click_button "Public"
-    click_button "Add first file!"
-    page.attach_file("file[]", 'spec/factories/files/happypanda.png')
-    click_button "Save changes"
-    expect(page).to have_selector("img[src$='happypanda.png']")
-    #TODO: move the following checks into another unit test after settling on a place for their functions.
-    project = Project.last
-    last_commit_id = project.barerepo.head.target_id
-    expect(File.exist?(project.thumbnail_for(last_commit_id,true))).to eq(true)
-  end
-
-  scenario "User uploads multiple files" do
+  scenario "User uploads multiple images" do
     sign_up_with("t@test.com","test1","secret12345")
     click_button "Create first project!"
     fill_in "project_name", :with => "testproject1"
@@ -60,17 +45,39 @@ feature "Projects" do
     expect(page).to have_selector("img[src$='naruto.png']")
   end
 
-  scenario "User sees logs for a project" do
-    sign_up_with("t@test.com","test1","secret12345")
-    click_button "Create first project!"
-    fill_in "project_name", :with => "testproject1"
-    click_button "Public"
-    click_button "Add first file!"
-    page.attach_file("file[]", 'spec/factories/files/happypanda.png')
-    click_button "Save changes"
-    click_link "Log"
-    expect(page).to have_link "Add new file happypanda.png"
-    last_commit_id = Project.last.barerepo.head.target_id
-    expect(page).to have_selector("img[src$='#{last_commit_id}']")
+  describe "After image upload" do
+
+    before :each do
+      sign_up_with("t@test.com","test1","secret12345")
+      click_button "Create first project!"
+      fill_in "project_name", :with => "testproject1"
+      click_button "Public"
+      click_button "Add first file!"
+      page.attach_file("file[]", 'spec/factories/files/happypanda.png')
+      click_button "Save changes"
+    end
+
+    scenario "User can see uploaded image" do
+      expect(page).to have_selector("img[src$='happypanda.png']")
+      #TODO: move the following checks into another unit test after settling on a place for their functions.
+      project = Project.last
+      last_commit_id = project.barerepo.head.target_id
+      expect(File.exist?(project.thumbnail_for(last_commit_id,true))).to eq(true)
+    end
+
+    scenario "User sees logs for a project" do
+      click_link "Log"
+      expect(page).to have_link "Add new file happypanda.png"
+      last_commit_id = Project.last.barerepo.head.target_id
+      expect(page).to have_selector("img[src$='#{last_commit_id}']")
+    end
+
+    scenario "User comments on a specific commit" do
+      click_link "Log"
+      click_link "Add new file happypanda.png"
+      fill_in "comment_body", :with => "test comment"
+      click_button "Create Comment"
+      expect(find('.comments')).to have_content("test comment")
+    end
   end
 end
