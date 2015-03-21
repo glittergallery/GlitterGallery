a9d4b84f983f0065882695ad91f00eadd1f7dff2
AhmedZaleh <ahmedzaleh7@gmail.com>
2015-03-21 21:05:49 +0200

    added validating presence of issue sub_id

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -8,6 +8,7 @@ class Issue < ActiveRecord::Base
   belongs_to :project
 
   validates_presence_of :title, :description, :user, :project, :type, :status
+  validates_presence_of :sub_id, unless: :set_sub_id
   validates :sub_id, uniqueness: { scope: :project }
 
   # TODO: make a list of 5 most popular types of issues
