4fe210bc07ffd40b44c72a8697d2c223629f3de7
Aditya Prakash <aditya.prakash132@gmail.com>
2015-06-26 23:37:30 +0530

    Test for user and project_member model

--- spec/models/project_member_spec.rb
+++ spec/models/project_member_spec.rb
@@ -1,5 +1,98 @@
 require 'spec_helper'
 
-RSpec.describe ProjectMember, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe 'ProjectMember' do
+  it 'has a valid factory' do
+    expect(create(:project_member)).to be_valid
+  end
+
+  it 'is invalid for undefined roles' do
+    expect(build(:project_member, role: 'rap_god')).to_not be_valid
+  end
+
+  it 'is invalid for duplicate entries' do
+    pm = create(:project_member)
+    expect(build(
+      :project_member,
+      member_id: pm.member_id,
+      gallery_id: pm.gallery_id
+    )).to_not be_valid
+  end
+
+  describe '.add_owner' do
+    let(:project) { create(:project) }
+    it 'adds user as owner' do
+      ProjectMember.add_owner project, project.user
+      expect(ProjectMember.last.member_id).to eq(project.user_id)
+    end
+  end
+
+  describe '.write_acess' do
+    before do
+      @project = create(:project)
+      @user = create(:user)
+    end
+
+    shared_examples 'has write acess roles' do |role|
+      before do
+        create(
+          :project_member,
+          member: @user,
+          member_project: @project,
+          role: role
+        )
+      end
+      it "true for #{role}" do
+        expect(ProjectMember.write_acess @project, @user).to be_truthy
+      end
+    end
+
+    it_behaves_like 'has write acess roles', 'owner'
+    it_behaves_like 'has write acess roles', 'collaborator'
+
+    shared_examples 'does not have write acess' do |role|
+      before do
+        create(
+          :project_member,
+          member: @user,
+          member_project: @project,
+          role: role
+        )
+      end if role
+      it "false for #{role}" do
+        expect(ProjectMember.write_acess @project, @user).to be_falsey
+      end
+    end
+
+    it_behaves_like 'does not have write acess', 'reporter'
+    it_behaves_like 'does not have write acess', nil
+  end
+
+  describe '.member?' do
+    before do
+      @project = create(:project)
+      @user = create(:user)
+    end
+
+    shared_examples 'it is member' do |role|
+      before do
+        create(
+          :project_member,
+          member: @user,
+          member_project: @project,
+          role: role
+        )
+      end
+      it "true for #{role}" do
+        expect(ProjectMember.member? @project, @user).to be_truthy
+      end
+    end
+
+    it_behaves_like 'it is member', 'owner'
+    it_behaves_like 'it is member', 'collaborator'
+    it_behaves_like 'it is member', 'reporter'
+
+    it 'returns false non existent members' do
+      expect(ProjectMember.member? @project, @user).to be_falsey
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,28 +2,63 @@ require 'spec_helper'
 
 describe 'User' do
   it 'has a valid factory' do
-    expect(FactoryGirl.create(:user)).to be_valid
+    expect(create(:user)).to be_valid
   end
 
   it 'is invalid without an email address' do
-    expect(FactoryGirl.build(:user, email: nil)).to_not be_valid
+    expect(build(:user, email: nil)).to_not be_valid
   end
 
   it 'is invalid without a correct email address' do
-    expect(FactoryGirl.build(:user, email: 'wrong@email,com')).to_not be_valid
-    expect(FactoryGirl.build(:user, email: 'wrong_at_email.com'))
+    expect(build(:user, email: 'wrong@email,com')).to_not be_valid
+    expect(build(:user, email: 'wrong_at_email.com'))
      .to_not be_valid
-    expect(FactoryGirl.build(:user, email: 'wrong.email@url.')).to_not be_valid
+    expect(build(:user, email: 'wrong.email@url.')).to_not be_valid
   end
 
   it 'is invalid with a duplicate email address' do
-    @user = FactoryGirl.create(:user, email: 'duplicate@email.com')
-    expect(FactoryGirl.build(:user, email: 'duplicate@email.com'))
+    @user = create(:user, email: 'duplicate@email.com')
+    expect(build(:user, email: 'duplicate@email.com'))
       .to_not be_valid
   end
 
   it 'is invalid without a username' do
-    expect(FactoryGirl.build(:user, username: nil)).to_not be_valid
-    expect(FactoryGirl.build(:user, username: '')).to_not be_valid
+    expect(build(:user, username: nil)).to_not be_valid
+    expect(build(:user, username: '')).to_not be_valid
+  end
+
+  describe '#owner?' do
+    before do
+      @user = create(:user)
+    end
+    context 'user is owner' do
+      it 'returns true' do
+        project = create(:project, user: @user)
+        expect(@user.owner? project).to be_truthy
+      end
+    end
+
+    context 'user is not owner' do
+      it 'returns false' do
+        project = create(:project)
+        expect(@user.owner? project).to be_falsey
+      end
+    end
+  end
+
+  describe '.search' do
+    let(:user1) { create(:user) }
+    let(:user2) { create(:user) }
+    context 'params was pased' do
+      it 'returns user' do
+        expect(User.search "#{user1.username}").to eq [user1]
+      end
+    end
+
+    context 'params is not passed' do
+      it 'returns all users' do
+        expect(User.search nil).to eq [user1, user2]
+      end
+    end
   end
 end
