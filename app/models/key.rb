cdd427193d901d1de289f13e8a4945d5273609ac
Aditya Prakash <aditya.prakash132@gmail.com>
2015-07-18 17:47:54 +0530

    Generate fingerprint on addition of ssh keys
    plagiarism source: https://github.com/gitlabhq/gitlabhq

--- app/models/key.rb
+++ app/models/key.rb
@@ -1,3 +1,40 @@
+require 'digest/md5'
+
 class Key < ActiveRecord::Base
+
   belongs_to :user
+
+  before_validation :strip_white_space, :generate_fingerprint
+
+  validates :title, presence: true, length: { within: 0..255 }
+  validates :key, presence: true,
+                  length: { within: 0..5000 },
+                  format: { with: /\A(ssh|ecdsa)-.*\Z/ },
+                  uniqueness: true
+  validates :key, format: { without: /\n|\r/, message: 'not a single line' }
+  validates :fingerprint, uniqueness: true,
+                          presence: { message: 'cannot be generated' }
+
+  def strip_white_space
+    self.key = key.strip unless key.blank?
+  end
+
+  # projects that has this key
+  def projects
+    user.authorized_projects
+  end
+
+  def shell_id
+    "key-#{id}"
+  end
+
+  private
+
+  def generate_fingerprint
+    self.fingerprint = nil
+
+    return unless key.present?
+
+    self.fingerprint = Gg::KeyFingerprint.new(key).fingerprint
+  end
 end
---
+++ lib/gg/key_fingerprint.rb
@@ -0,0 +1,55 @@
+module Gg
+  class KeyFingerprint
+    include Gg::Popen
+
+    attr_accessor :key
+
+    def initialize(key)
+      @key = key
+    end
+
+    def fingerprint
+      cmd_status = 0
+      cmd_output = ''
+
+      Tempfile.open('gitlab_key_file') do |file|
+        file.puts key
+        file.rewind
+
+        cmd = []
+        cmd.push *%W(ssh-keygen)
+        cmd.push *%W(-E md5) if explicit_fingerprint_algorithm?
+        cmd.push *%W(-lf #{file.path})
+
+        cmd_output, cmd_status = popen(cmd, '/tmp')
+      end
+      puts cmd_status, cmd_output
+      return nil unless cmd_status.zero?
+
+      # 16 hex bytes separated by ':', optionally starting with "MD5:"
+      fingerprint_matches = cmd_output.match(/(MD5:)?(?<fingerprint>(\h{2}:){15}\h{2})/)
+      return nil unless fingerprint_matches
+
+      fingerprint_matches[:fingerprint]
+    end
+
+    private
+
+    def explicit_fingerprint_algorithm?
+      # OpenSSH 6.8 introduces a new default output format for fingerprints.
+      # Check the version and decide which command to use.
+
+      version_output, version_status = popen(%W(ssh -V))
+      return false unless version_status.zero?
+
+      version_matches = version_output.match(/OpenSSH_(?<major>\d+)\.(?<minor>\d+)/)
+      return false unless version_matches
+
+      if version_matches[:major].to_i > 6 && version_matches[:minor].to_i > 8
+        return true
+      else
+        return false
+      end
+    end
+  end
+end
