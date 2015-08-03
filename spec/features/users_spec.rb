521728f009df9a8adc37588cd52c909de4159fdb
Aditya Prakash <aditya.prakash132@gmail.com>
2015-08-03 13:36:37 +0530

    Add feature test of key

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -51,4 +51,43 @@ feature 'Users' do
       expect(page.current_path).to eq(new_user_session_path)
     end
   end
+
+  describe 'keys' do
+    let(:user) { create(:user) }
+    before do
+      login_as(user)
+      allow_any_instance_of(Key).to receive(:add_to_shell).and_return(true)
+    end
+
+    scenario 'adds key to profile' do
+      key =
+        'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCx3ke+rnMT/ILY81K1un1CWf9ghcP' +
+        'glIlV7pMV2H5AwyC/Dx5x+DyKmNmhBmvCYJ+1we8f0pPXLx2QpyAXw8s0s+sBL/gkiz' +
+        'sqqwrUzK9Rlkj58kvNFl8gLQk3qqs8dR6bODP9LQqCGhMFErQtDQTvBq91jhWuIIunu' +
+        'mK7T+0GWDMf7O9CNdr/aprYrUfuGLggOdz0oPja792V+ay1xWAHEOueKfGvOGFDbQlc' +
+        'TT2uI9wYz9RGkLhDNOo4S74W59xMwMpf77XsoTYxcdrAT7WpTlzaj2usbbGBgcBKx5k' +
+        'b0dPBOQ3rQadtZnLjN2dZAeapUO2MElyX0lxt1nrbIKC2 addie@host.localdomain'
+      visit '/keys'
+      fill_in 'key[title]', with: 'test'
+      fill_in 'key[key]', with: key
+      expect{click_button 'Add key'}.to change{Key.all.count}.by(1)
+    end
+
+    context 'key page' do
+      before do
+        @key = create(:key, user: user)
+        visit '/keys'
+      end
+
+      it 'sees key' do
+        expect(find('table')).to have_content(@key.title)
+      end
+
+      it 'removes key' do
+        allow_any_instance_of(Key).to receive(:remove_from_shell).and_return(true)
+        find('table').click_link 'Remove'
+        expect(find('.option')).not_to have_content(@key.title)
+      end
+    end
+  end
 end
