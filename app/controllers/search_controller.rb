f33b0c298dd3b39acff9905ea6e78190ab738a37
Aditya Prakash <aditya.prakash132@gmail.com>
2015-08-29 10:45:31 +0530

    Show thumbnails of files on search page

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,15 +1,19 @@
 class SearchController < ApplicationController
-  before_filter :return_context
 
+  # GET /search
   def show
+    if params[:id].present?
+      @user = User.find_by username: params[:user_id]
+      render_404 && return if @user.blank?
+      @project = Project.with_deleted.find_by user_id: @user.id,
+                                              name: params[:id]
+    end
 
-  end
-
-  private
-  def return_context
-    @user = User.find_by username: params[:user_id]
-    render_404 && return if @user.blank?
-    @project = Project.with_deleted.find_by user_id: @user.id,
-                                            name: params[:id]
+    if @project
+      @images, @directories = Gg::Search.find_files(params[:search], @project)
+      render 'project_show'
+    else
+      render 'show'
+    end
   end
 end
---
+++ lib/gg/search.rb
@@ -0,0 +1,23 @@
+module Gg
+  module Search
+    extend self
+
+    # Find files in satellitedir of project which match the
+    # given query (with ignore case). Allow delimiters are
+    # space and comma.
+    def find_files(query, project)
+      images = []
+      dir = []
+      path = File.join project.satellitedir, '/**/*'
+      files = Dir[path]
+      query = query.gsub(/[ ,]+/, '|')
+      files.each do |f|
+        next if (/#{query}/i =~ f).nil?
+        f_name = f.sub(project.satellitedir + '/', '')
+        push = {name: f_name, path: f.sub('public/', '')}
+        File.file?(f) ? images << push : dir << push
+      end
+      [images, dir]
+    end
+  end
+end
